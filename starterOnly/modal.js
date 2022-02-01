960ae6865e87d36943efbf127643d48c676f21ff
LuciaBOURQUE <80245094+LuciaBOURQUE@users.noreply.github.com>
2022-02-01 10:12:42 +0100

    Ajout fonction case à cocher

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -110,19 +110,35 @@ function quantityInput() {
 
 
 
-// Checkbox - 1 choix à sélectionner > À REVOIR
+// Checkbox - 1 choix à sélectionner
 function checkboxInput() {
-  let checkbox = document.forms["myForm"]('input[name="location"]:checked').value;
-//  let checkbox = document.querySelector('input[name="location"]:checked').value;
-  if (!checkbox){
-    document.querySelector('.checkbox-error').style.opacity = "1";
-    return false;
-  } else {
-    document.querySelector('.checkbox-error').style.opacity = "0";
-    return true;
+let checkbox = document.querySelectorAll(".checkbox-input");
+
+// Si une des cases est coché (trouver une formule plus optimale)
+if(checkbox[0].checked || checkbox[1].checked
+  || checkbox[2].checked || checkbox[3].checked
+  || checkbox[4].checked || checkbox[5].checked){ 
+  document.querySelector('.checkbox-error').style.opacity = "0";
+  return true;
+} else {
+  document.querySelector('.checkbox-error').style.opacity = "1";
+  return false;
   }
 }
 
+
+
+// Checkbox conditions - La case 1 est obligatoire l'autre optionnel
+function conditionInput() {
+  let checkboxCondition = document.getElementById("checkbox1");
+  if(!checkboxCondition.checked) { 
+    document.querySelector('.condition-error').style.opacity = "1";
+    return false;
+  } else {
+    document.querySelector('.condition-error').style.opacity = "0";
+    return true;
+    }
+  }
 
 
 // Action SUBMIT par le Formulaire
@@ -133,6 +149,7 @@ function validateForm() {
   dateInput();
   quantityInput();
   checkboxInput();
+  conditionInput();
 }
 
 
@@ -142,12 +159,14 @@ btnSubmit.addEventListener("click", e => {
   let valid = validateForm();
   if(!valid)  {
     return;
-  } // SINON(else): Prendre la fonction en bas
+  } else {
+    lauchModalSucess();
+  }
 })
 
 // PARTIE C - Création du bouton "Submit" (reprendre modal ouverture/fermeture)
 const modalSucess = document.getElementsByClassName("bground-check");
-function lauchModalSucess() { // + Ajout du ".text-control"
+function lauchModalSucess() {
   modalSucess.style.display = "block";
 }
 /*
